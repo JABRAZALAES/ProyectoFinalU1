c14bf09014b92af65cae47606a24e8e7d4dfb816
Liseth-Poma <lcpoma@espe.edu.ec>
2023-12-19 15:33:18 -0500

    commit5byliss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>Tiendita - ESPE</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -11,7 +11,14 @@
     <style>
         body {
             font-family: 'Nunito';
-            
+            margin: 0; /* Eliminar el margen predeterminado del cuerpo */
+        }
+
+        footer {
+            background-color: #4CAF50; /* Color verde */
+            color: white;
+            text-align: center;
+            padding: 1em;
         }
 
         nav {
@@ -20,14 +27,15 @@
             align-items: center;
             padding: 20px;
             background-color: rgba(255, 255, 255, 0.7);
-            backdrop-filter: blur(10px); 
+            backdrop-filter: blur(10px);
         }
 
-        img{
-            height: 100%;
-            width: 100%;
+        img {
+            max-width: 100%; /* Establecer el ancho máximo al 100% del contenedor */
+            height: auto; /* Hacer que la altura se ajuste automáticamente para mantener la proporción */
+            display: block; /* Eliminar espacio adicional debajo de la imagen */
+            margin: 0 auto; /* Centrar la imagen dentro de su contenedor */
         }
-        
 
         .logo {
             font-size: 24px;
@@ -38,11 +46,10 @@
             display: flex;
             gap: 10px;
         }
-    
 
         .right-links a {
             text-decoration: none;
-            color: #2d3748;
+            color: #4CAF50;
             font-weight: 600;
             padding: 8px 16px;
             border: 1px solid #4a5568;
@@ -51,35 +58,33 @@
         }
 
         .right-links a:hover {
-            background-color: #4a5568;
+            background-color: #4CAF50;
             color: #ffffff;
-        }
-
-        h1 {
-            border: 2px solid #4a5568;
-            padding: 10px;
-            display: inline-block;
         }
     </style>
 </head>
 <body class="antialiased">
-    
-    <nav>
-        <div class="logo">Minimarket</div>
-        <div class="right-links">
-            @auth
-                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-            @else
-                <a href="{{ route('login') }}">Iniciar Sesión</a>
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Registrarse</a>
-                @endif
+
+<nav>
+    <div class="logo">Tiendita - ESPE</div>
+    <div class="right-links">
+        @auth
+            <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Inicio</a>
+        @else
+            <a href="{{ route('login') }}">Iniciar Sesión</a>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Registrarse</a>
             @endif
-        </div>
-    </nav>
-    <div class="container">
-        <img src="https://th.bing.com/th/id/OIG.dXt1aq0.uqpEL.Fi5_9E?w=1024&h=1024&rs=1&pid=ImgDetMain">
+        @endif
     </div>
+</nav>
+<div class="container">
+    <img src="https://th.bing.com/th/id/OIG.dXt1aq0.uqpEL.Fi5_9E?w=1024&h=1024&rs=1&pid=ImgDetMain">
+</div>
+
+<footer>
+    <p>Tiendita - ESPE - © {{ date('Y') }} Minimarket</p>
+</footer>
 
 </body>
 </html>
